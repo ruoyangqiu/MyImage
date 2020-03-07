9d7ae65f0e053ce1e1f3cdbd6f88ae7e31f5433a
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-06 23:56:28 -0800

    Show Image for get

--- MyImage/Controllers/WeatherForecastController.cs
+++ MyImage/Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyImage.ImageProcessor;
 
 namespace MyImage.Controllers
 {
@@ -34,6 +37,26 @@ namespace MyImage.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("image")]
+        public ActionResult GetImage()
+        {
+            //PictureBox pictureBox1 = new PictureBox();
+            IMyImage img = new MyImageType("Resource\\Sunset.jpg");
+            //string url = img.RotationByAngle(70);
+            //var image = System.IO.File.OpenRead(url);
+            Image i1 = img.RotationByAngle(90);
+            Byte[] b;
+            b = ImageToByteArray(i1);
+            return File(b, "image/png");
+        }
+
+        private byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
         }
     }
 }
