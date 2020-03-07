47d0e8890a6f49235b98e84d18c66e3450672151
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-06 23:57:31 -0800

    Added interface to IMyImage

--- MyImage/ImageProcessor/IMyImage.cs
+++ MyImage/ImageProcessor/IMyImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace MyImage.ImageProcessor
     {
         void display();
 
- 
+        Image RotationByAngle(int angle);
+
+        string Rotation();
     }
 }
--- MyImage/Startup.cs
+++ MyImage/Startup.cs
@@ -58,7 +58,7 @@ namespace MyImage
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
