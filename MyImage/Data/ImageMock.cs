c5f1b561db0058c88d24868b2d7b6fce4d1c6b86
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-07 19:58:07 -0800

    Made Singleton ImageMock

---
+++ MyImage/Data/ImageMock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyImage.Data
+{
+    public class ImageMock
+    {
+        #region Singleton
+        private static volatile ImageMock instance;
+        private static readonly object syncRoot = new Object();
+
+        public static ImageMock Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new ImageMock();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
+        public string url { get; set; }
+
+        public void add(string u)
+        {
+            url = u;
+        }
+    }
+}
