92d01fb6d9b598f0d17d024926852665cf0f9062
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-07 19:57:43 -0800

    Added MyImageService

---
+++ MyImage/Service/MyImageServer.cs
@@ -0,0 +1,13 @@
+﻿using MyImage.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyImage.Service
+{
+    public class MyImageServer
+    {
+        public ImageMock datastore = ImageMock.Instance;
+    }
+}
