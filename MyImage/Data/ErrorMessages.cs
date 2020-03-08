0a6e07bfffab0aba63b7cc1f5d2d6126a038d2b4
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-08 16:19:01 -0700

    Added ErrorMessage.cs and InvalidUriError to it

---
+++ MyImage/Data/ErrorMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyImage.Data
+{
+    public class ErrorMessages
+    {
+        public class InvalidUriError
+        {
+            public int ErrorCode { get => 100; }
+
+            public string Message { get => "The URI is Invalid!"; }
+        }
+    }
+}
