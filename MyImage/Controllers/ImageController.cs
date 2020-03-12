c7ef564ac08f5938610c3deae4f9ed521f99b3d9
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-11 21:16:59 -0700

    Added comments to Image Controller

--- MyImage/Controllers/ImageController.cs
+++ MyImage/Controllers/ImageController.cs
@@ -21,7 +21,11 @@ namespace MyImage.Controllers
         }
 
         
-
+        /// <summary>
+        /// Upload Image
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         [HttpPost]
         [ProducesResponseType(200)]
         [ProducesResponseType(typeof(InvalidUriError), 400)]
@@ -39,6 +43,11 @@ namespace MyImage.Controllers
             return StatusCode(200);
         }
 
+
+        /// <summary>
+        /// Get the Image
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [ProducesResponseType(200)]
         [ProducesResponseType(typeof(NoImageError), 400)]
@@ -52,7 +61,11 @@ namespace MyImage.Controllers
         }
 
         
-
+        /// <summary>
+        /// Flip the image either horizontla or vertical based on user's input
+        /// </summary>
+        /// <param name="orientation"></param>
+        /// <returns></returns>
         [HttpPut("flipping")]
         [ProducesResponseType(200)]
         [ProducesResponseType(typeof(NoImageError), 400)]
@@ -76,6 +89,11 @@ namespace MyImage.Controllers
             return StatusCode(200);
         }
 
+
+        /// <summary>
+        /// Make the grey scale of the image
+        /// </summary>
+        /// <returns></returns>
         [HttpPut("grayscale")]
         [ProducesResponseType(200)]
         [ProducesResponseType(typeof(NoImageError), 400)]
@@ -89,6 +107,12 @@ namespace MyImage.Controllers
             return StatusCode(200);
         }
 
+        /// <summary>
+        /// Resize the image depend on the imput of user
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
         [HttpPut("resizing")]
         [ProducesResponseType(typeof(NoImageError), 400)]
         [ProducesResponseType(typeof(InvalidWidthError), 400)]
@@ -114,7 +138,10 @@ namespace MyImage.Controllers
         }
 
        
-
+        /// <summary>
+        /// Rotate Image 90 degree counterclockwise
+        /// </summary>
+        /// <returns></returns>
         [HttpPut("leftrotation")]
         [ProducesResponseType(typeof(NoImageError), 400)]
         public ActionResult LeftRotation()
@@ -127,6 +154,10 @@ namespace MyImage.Controllers
             return StatusCode(200);
         }
 
+        /// <summary>
+        /// Rotate Image 90 degree clockwise
+        /// </summary>
+        /// <returns></returns>
         [HttpPut("rightrotation")]
         [ProducesResponseType(typeof(NoImageError), 400)]
         public ActionResult RightRotation()
@@ -139,6 +170,14 @@ namespace MyImage.Controllers
             return StatusCode(200);
         }
 
+        /// <summary>
+        /// Rotate image for any angle
+        /// if angle is positive, rotate clockwise
+        /// if angle is negative, rotate counter-clockwise
+        /// if angle is zero, iimage won't change
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <returns></returns>
         [HttpPut("anglerotation")]
         [ProducesResponseType(200)]
         [ProducesResponseType(typeof(NoImageError), 400)]
@@ -156,6 +195,10 @@ namespace MyImage.Controllers
             return StatusCode(200);
         }
 
+        /// <summary>
+        /// Generate a thumbnail for image
+        /// </summary>
+        /// <returns></returns>
         [HttpPut("thumbnail")]
         [ProducesResponseType(typeof(NoImageError), 400)]
         public ActionResult Thumbnail()
@@ -168,6 +211,10 @@ namespace MyImage.Controllers
             return StatusCode(200);
         }
 
+        /// <summary>
+        /// Get the thumbnail
+        /// </summary>
+        /// <returns></returns>
         [HttpGet("thumbnail")]
         [ProducesResponseType(typeof(NoImageError), 400)]
         public ActionResult GetThumbnail()
